55d99d6fefa490844736e771e07dedb7262d8274
diegovelasquez04 <diegovelasquez04@gmail.com>
2018-08-18 21:59:45 -0500

    Video del 3 al 10

---
+++ Sales/Sales/Infrastructure/InstaLocator.cs
@@ -0,0 +1,12 @@
+﻿namespace Sales.Infrastructure
+{
+    using ViewModels;
+   public class InstaLocator
+    {
+        public MainViewModel Main { get; set; }
+        public InstaLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+    }
+}
