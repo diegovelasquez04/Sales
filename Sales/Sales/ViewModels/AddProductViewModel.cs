e033ad10cea681eea25be5c0baa8b43531054c15
diegovelasquez04 <diegovelasquez04@gmail.com>
2018-08-27 23:13:39 -0500

    Video  21-22

--- Sales/Sales/ViewModels/AddProductViewModel.cs
+++ Sales/Sales/ViewModels/AddProductViewModel.cs
@@ -1,11 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Sales.ViewModels
+﻿namespace Sales.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
+    using Sales.Helpers;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+
     public class AddProductViewModel : BaseViewModel
     {
+        #region Attributes
+        public bool isRunning;
+        public bool isEnable;
 
+        #endregion
+
+
+        #region Propperties
+        public string Descriprion { get; set; }
+        public string Price { get; set; }
+        public string Remarks { get; set; }
+        public bool IsRunning
+        {
+            get { return this.isRunning; }
+            set { this.SetValue(ref this.isRunning, value); }
+        }
+        public bool IsEnable
+        {
+            get { return this.isEnable; }
+            set { this.SetValue(ref this.isEnable, value); }
+        }
+        #endregion
+
+        #region Constructors
+        public AddProductViewModel()
+        {
+            this.IsEnable = true;
+        }
+        #endregion
+
+        #region Commands
+        public ICommand SaveCommand
+        {
+            get
+            {
+                return new RelayCommand(Save);
+            }
+        }
+
+        private async void Save()
+        {
+            if(string.IsNullOrEmpty(this.Descriprion))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    Languages.DescriptionError,
+                    Languages.Accept);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Price))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    Languages.PriceError,
+                    Languages.Accept);
+                return;
+            }
+
+            var price = decimal.Parse(this.Price);
+            if (price < 0)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    Languages.PriceError,
+                    Languages.Accept);
+                return;
+            }
+
+        }
+        #endregion
     }
 }
