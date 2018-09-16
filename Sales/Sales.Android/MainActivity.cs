07d7d5ca0b84db0146519f13b8b914d7b832267e
diegovelasquez04 <diegovelasquez04@gmail.com>
2018-09-15 23:43:16 -0500

    Revert "Video 26"
    This reverts commit 6324357ee3ad598e3dc073c0cea0f12b70ef2ec4.

--- Sales/Sales.Android/MainActivity.cs
+++ Sales/Sales.Android/MainActivity.cs
@@ -1,38 +1,26 @@
-﻿namespace Sales.Droid
-{
-    using Android.App;
-    using Android.Content.PM;
-    using Android.OS;
-    using Android.Runtime;
-    using Plugin.CurrentActivity;
-    using Plugin.Permissions;
+﻿using System;
 
-    [Activity(Label = "Sales",
-              Icon = "@mipmap/icon",
-              Theme = "@style/MainTheme",
-              MainLauncher = true, 
-              ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Sales.Droid
+{
+    [Activity(Label = "Sales", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
-            base.OnCreate(savedInstanceState);
-            CrossCurrentActivity.Current.Init(this, savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
-        }
-        public override void OnRequestPermissionsResult(
-          int requestCode,
-          string[] permissions,
-          [GeneratedEnum] Permission[] grantResults)
-        {
-            PermissionsImplementation.Current.OnRequestPermissionsResult(
-                requestCode,
-                permissions,
-                grantResults);
         }
     }
 }
